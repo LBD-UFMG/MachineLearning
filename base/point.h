524cbf53108b72b395010ae62356afd5cfee1fc9
gcmgomes <guilherme.cm.gomes@gmail.com>
2016-07-18 16:59:19 -0300

    point interface implementation

---
+++ base/point.h
@@ -0,0 +1,49 @@
+#ifndef _BASE_POINT_H_
+#define _BASE_POINT_H_
+/**
+ * \file point.h
+ * \brief Header defining the multi-dimensional point interface.
+ */
+
+/**
+ * Basic objects to be used by all machine learning algorithms.
+ */
+namespace base {
+
+/**
+ * \brief Multi-dimensional object interface.
+ *
+ * This interface is the primitive input data type for the learning algorithms.
+ * For every new type of object we want to learn from, we must implement a
+ * suitable concrete class to handle it.
+ */
+class Point {
+ public:
+   /**
+    * Default constructor.
+    */
+   Point() = 0;
+  /** String parsing constructor.
+   *
+   * \param point_string should be equal to the return value of ToString().
+   */
+  Point(const std::string& point_string) = 0;
+  /** General accessor method for the underlying data.
+   * \param feature_id The coordinate of interest. Indexing is implementation
+   * specific.
+   * \return The value of the coordinate specified in the input.
+   */
+  double GetFeatureValue(unsigned feature_id) = 0 const;
+
+  /**
+   * Builds a `std::string` representation of the object.
+   *
+   * \return A `std::string` object usable by
+   * \link Point::Point(const std::string& point_string) \endlink.
+   */
+  std::string ToString() = 0 const;
+};
+
+}  // namespace base
+
+#endif
