7c9e8babc7127ad4189e454744de2f43a695f0a0
clebsonc <clebsondm@gmail.com>
2016-07-27 15:49:27 -0300

    Criterion as the  most abstract as possible.

---
+++ tree/criterion.hpp
@@ -0,0 +1,30 @@
+#ifndef MACHINELEARNING_TREE_CRITERION_HPP
+#define MACHINELEARNING_TREE_CRITERION_HPP
+
+namespace tree {
+
+/**
+ * @brief Base class that defines the many criterious of splitting.
+ *
+ * This interface provides some methods that are used to compute the impurity of a given Node object.
+ * Some impurity measures are Gini, Entrophy and Missclassification Error Rate for classification problens and MSE for regression
+ * problems.
+ */
+class Criterion{
+protected:
+  /**
+   * \brief Standard destructor.
+   */
+  virtual ~Criterion();
+
+  /**
+   * \brief Compute the Gini index impurity measure for the given node.
+   * \param node contains the representation of points that will be used to compute the impurity.
+   * \return the impurity of the data in the given node.
+   */
+  virtual double ImpurityMeasure(const Node & node) const = 0;
+};
+
+} // namespace tree
+
+#endif // MACHINELEARNING_TREE_CRITERION_HPP
