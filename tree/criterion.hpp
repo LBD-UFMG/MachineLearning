7f4a737695a6b9937eb9f4bdf3da81d8528f2456
clebsonc <clebsondm@gmail.com>
2016-07-27 22:26:33 -0300

    Fixed all coments

--- tree/criterion.hpp
+++ tree/criterion.hpp
@@ -4,7 +4,7 @@
 namespace tree {
 
 /**
- * @brief Base class that defines the many criterious of splitting.
+ * \brief Base class that defines the many criterious of splitting.
  *
  * This interface provides some methods that are used to compute the impurity of a given Node object.
  * Some impurity measures are Gini, Entrophy and Missclassification Error Rate for classification problens and MSE for regression
@@ -18,11 +18,11 @@ protected:
   virtual ~Criterion();
 
   /**
-   * \brief Compute the Gini index impurity measure for the given node.
+   * \brief Compute the impurity measure for the given node.
    * \param node contains the representation of points that will be used to compute the impurity.
    * \return the impurity of the data in the given node.
    */
-  virtual double ImpurityMeasure(const Node & node) const = 0;
+  virtual double Impurity(const Node & node) const = 0;
 };
 
 } // namespace tree
