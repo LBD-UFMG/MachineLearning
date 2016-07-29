bc39f8ab35907b9faa2a336206888292c7bf2748
clebsonc <clebsondm@gmail.com>
2016-07-29 10:29:05 -0300

    Lenght line at maximum of 80 characters

--- tree/criterion.hpp
+++ tree/criterion.hpp
@@ -6,8 +6,10 @@ namespace tree {
 /**
  * \brief Base class that defines the many criterious of splitting.
  *
- * This interface provides some methods that are used to compute the impurity of a given Node object.
- * Some impurity measures are Gini, Entrophy and Missclassification Error Rate for classification problens and MSE for regression
+ * This interface provides some methods that are used to compute the impurity
+ * of a given Node object.
+ * Some impurity measures are Gini, Entrophy and Missclassification Error Rate
+ * for classification problens and MSE for regression
  * problems.
  */
 class Criterion{
@@ -19,7 +21,8 @@ protected:
 
   /**
    * \brief Compute the impurity measure for the given node.
-   * \param node contains the representation of points that will be used to compute the impurity.
+   * \param node contains the representation of points that will be used to
+   * compute the impurity.
    * \return the impurity of the data in the given node.
    */
   virtual double Impurity(const Node & node) const = 0;
