1e200b4ca25dcc325c112c0ce4524c2eb68499dd
Raphael Campos <raphaelrcampos@yahoo.com.br>
2016-07-30 19:38:36 -0300

    defining learner interface

---
+++ base/learner.h
@@ -0,0 +1,45 @@
+#ifndef  _MACHINELEARNING_BASE_LEARNER_H_ 
+#define  _MACHINELEARNING_BASE_LEARNER_H_
+
+#include <vector>
+
+namespace base {
+
+/**
+ * Base class for all learning algorithm implementations.
+ */
+class Learner{
+
+  public:
+
+    /**
+     * \brief Standard destructor.
+     */
+    virtual ~Learner();
+
+    /**
+     * Train the learner with a given dataset.
+     * \param X A pointer object of Collection.
+     */
+    virtual void Train(/*const Collection *X*/);
+
+    /**
+     * Predicts the labels of the instances of X based on the fitted model.
+     * \param X           A pointer object of Collection.
+     * \param prediction  Instances prediction.
+     */
+    virtual void Predict(/*const Collection *X,*/ std::vector<double> &prediction) = 0;
+
+    /**
+     *  Returns the predicted probability distribution of each instance in X
+     *  over all possible classes.
+     *  \param X            A pointer object of Collection.
+     *  \param distribution Instances probability distribution.
+     */
+    virtual void PredictProbability(/*const Collection *X,*/
+                              std::vector<std::vector<double> > &distribution) = 0;
+};
+
+} // namespace base
+
+#endif
