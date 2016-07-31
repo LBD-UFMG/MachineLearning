6a18d0fab3f0aecd65600a5ed2b810786b84344c
Raphael Campos <raphaelrcampos@yahoo.com.br>
2016-07-31 09:56:44 -0300

    updating .cpp methods definition

--- tree/tree.cpp
+++ tree/tree.cpp
@@ -2,7 +2,7 @@
 
 namespace tree {
 
-Tree::Tree(Splitter *splitter, /*Criterion *criterion, Sampler *sampler,*/ float max_features,
+Tree::Tree(Splitter *splitter, /*Criterion *criterion,*/ float max_features,
             int max_depth, int max_leaf_nodes, int min_samples_leaf, int min_samples_split){
     // code here
 }
@@ -12,12 +12,12 @@ Tree::~Tree(){
 }
 
 void Tree::Build(/*const Collection *X,*/
-            const std::vector<double> *sample_weight,
-            const std::vector<double> *feature_weight) {
+            const std::vector<double> &sample_weight,
+            const std::vector<double> &feature_weight) {
   // code here
 }
 
-void Tree::Traverse(/*const Collection *X, std::vector<Node*> &leafs*/){
+void Tree::Traverse(/*const Collection *X, std::vector<Node*> *leafs*/){
   // code here
 }
 
