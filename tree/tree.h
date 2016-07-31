66f2ba8dfbecdbcdb59f498ceff624a4ce17b3f2
Raphael Campos <raphaelrcampos@yahoo.com.br>
2016-07-31 09:55:28 -0300

    fixing cosideration of code review

--- tree/tree.h
+++ tree/tree.h
@@ -16,17 +16,16 @@ class Tree{
   public:
 
     /**
-     * Tree class constructor
-     * \param splitter      Splitter object
-     * \param criterion     Criterion object
-     * \param sampler       Sampler object
+     * Tree class constructor.
+     * \param splitter      Splitter object.
+     * \param criterion     Criterion object.
      * \param max_features    The maximum number of features used to create the tree.
      * \param max_depth     The maximum depth of the tree.
      * \param max_leaf_nodes  The maximum number of leaf nodes of the tree.
      * \param min_samples_leaf  The minimum number of samples per leaf.
      * \param min_samples_split The minimum number of samples per node in order to be splitted in two.
      */
-    Tree(Splitter *splitter, /*Criterion *criterion, Sampler *sampler,*/ float max_features,
+    Tree(Splitter *splitter, /*Criterion *criterion,*/ float max_features,
        int max_depth, int max_leaf_nodes, int min_samples_leaf, int min_samples_split);
 
     /**
@@ -41,7 +40,7 @@ class Tree{
      * \param feature_weight Weight of each feature .
      */
     void Build(/*const Collection *X,*/
-          const std::vector<double> *sample_weight, const std::vector<double> *feature_weight);
+          const std::vector<double> &sample_weight, const std::vector<double> &feature_weight);
 
     
     /**
@@ -49,7 +48,7 @@ class Tree{
      * \param X     An object of Collection.
      * \param leafs Vector of leafs where the instances laid down.
      */
-    void Traverse(/*const Collection *X, std::vector<Node*> &leafs*/);
+    void Traverse(/*const Collection *X, std::vector<Node*> *leafs*/);
 
   protected:
 
