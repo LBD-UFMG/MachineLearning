28250b78fad0c1b69180606a2f75a34faa27ee93
luizfgoncalves <luizfgoncalves@gmail.com>
2016-09-12 13:53:34 -0300

    Collection fixes

--- base/collection.h
+++ base/collection.h
@@ -1,3 +1,5 @@
+
+
 #ifndef MACHINELEARNING_BASE_COLLECTION_H_
 #define MACHINELEARNING_BASE_COLLECTION_H_
 #include <string>
@@ -34,44 +36,21 @@ class Collection {
    * specific.
    * \return A pointer for the specified point in the collection.
    */
-//  virtual *Point GetPoint(unsigned int point_id) const = 0;
-
-  /** General accessor method for a `std::string` representation of 
-   * each point of the collection.
-   * \param point_id The point of interest. 
-   * \return A `std::string` representation of the specified point.
-   */
-  virtual std::string GetStringPoint(unsigned int point_id) const = 0;
-
-  /** Returns the value of a specified feature from a specified 
-   * point of the collection.
-   * \param point_id The point of interest. 
-   * \param feature_id The feature of interest. 
-   * \return The value of the feature of the specified point
-   */
-  virtual double GetPointFeatureValue(unsigned int point_id,
-                   unsigned int feature_id) const = 0;
+  virtual const Point* GetPoint(unsigned int point_id) const = 0;
 
   /** Creates a std::vector with all the possible values of
-   * the specified feature in the collection.
-   * \param feature_id The point of interest. 
+   * the labels in the collection.
    * \param feature_values A `std::vector` structure that will
-   * store all possible feature values in the collection.
+   * store all possible label values in the collection.
    */
-  virtual void GetFeatureValues(unsigned int feature_id, 
-                 std::vector<double> *feature_values) const = 0;
+  virtual void GetLabels(std::vector<double> *label_values) const = 0;
 
-  /**
-   * \brief Returns the numbers of points in the collection.
-   * \return Number of points.
+  /** Returns the label value of the specified point
+   * the labels in the collection.
+   * \param point_id The point of interest.
+   * \return Label value of the desired point.
    */
-  virtual unsigned int GetPointsNumber() const = 0;
-
-  /**
-   * \brief Returns the numbers of features in the collection.
-   * \return Number of features.
-   */
-  virtual unsigned int GetFeaturesNumber() const = 0;
+  virtual double GetLabel(unsigned int point_id) const = 0;
 };
 
 }  // namespace base
